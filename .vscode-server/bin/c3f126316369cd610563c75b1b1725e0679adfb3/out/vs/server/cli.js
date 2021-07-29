87c93907a027e9cdc91d45c7a3b30936d16684a4
Lucas Achaval <achaval.lucas@gmail.com>
2021-07-29 11:43:32 -0300

    .gitignore fix

--- .vscode-server/bin/c3f126316369cd610563c75b1b1725e0679adfb3/out/vs/server/cli.js
+++
@@ -1,3 +0,0 @@
-const path=require("path");delete process.env.ELECTRON_RUN_AS_NODE,process.env.VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH=process.env.VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH||path.join(__dirname,"..","..","..","remote","node_modules"),require("../../bootstrap-node").injectNodeModuleLookupPath(process.env.VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH),require("../../bootstrap-amd").load("vs/server/remoteCli");
-
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c3f126316369cd610563c75b1b1725e0679adfb3/core/vs/server/cli.js.map
--- .vscode-server/bin/c3f126316369cd610563c75b1b1725e0679adfb3/out/vs/server/uriTransformer.js
+++
@@ -1,3 +0,0 @@
-module.exports=function(t){return{transformIncoming:e=>e.scheme==="vscode-remote"?{scheme:"file",path:e.path}:e.scheme==="file"?{scheme:"vscode-local",path:e.path}:e,transformOutgoing:e=>e.scheme==="file"?{scheme:"vscode-remote",authority:t,path:e.path}:e.scheme==="vscode-local"?{scheme:"file",path:e.path}:e,transformOutgoingScheme:e=>e==="file"?"vscode-remote":e==="vscode-local"?"file":e}};
-
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c3f126316369cd610563c75b1b1725e0679adfb3/core/vs/server/uriTransformer.js.map
